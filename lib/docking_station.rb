40c7c57d4b28ec06a989b8955be635f510eaa928
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-19 16:27:44 +0100

    Defined full and empty into private section - refactored 20 into constant

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,30 @@
 class DockingStation
   attr_reader :docks
 
+  DEFAULT_CAPACITY = 20
+
   def initialize
     @docks = []
   end
 
   def release_bike
-    raise "No bikes available" if @docks.empty?
-    Bike.new
+    raise "No bikes available" if empty?
+    @docks.pop
   end
 
   def dock(bike)
-    raise "Dock is full" if @docks.count >= 20
+    raise "Dock is full" if full?
     @docks << bike
+  end
+
+  private
+
+  def full?
+    @docks.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @docks.empty?
   end
 
 end
